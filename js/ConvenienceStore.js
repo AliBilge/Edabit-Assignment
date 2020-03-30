72fe745e0fc46e7a6e343e7926705723892bf40f
Ali <alibilge@yahoo.ca>
2020-03-30 09:29:25 -0600

    Convenience Store Assignment

--- js/ConvenienceStore.js
+++ js/ConvenienceStore.js
@@ -1,5 +1,8 @@
+// Conveniece Strore challenge.
+// This is a function that finds if the change equal or bigger than due amount.
+
 function changeEnough(change, amountDue) {
-	var [a, b, c, d] = change;
-	amountDue <= a * 0.25 + b * 0.10 + c * 0.05 + d * 0.01
+	var [a, b, c, d] = change; // Creating array for the "change" variable.
+	amountDue <= a * 0.25 + b * 0.10 + c * 0.05 + d * 0.01 // Converts every thing to DOLLAR.
 	return true;
 }
