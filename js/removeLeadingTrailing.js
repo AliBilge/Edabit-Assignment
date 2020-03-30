29a2808919d4405823b9842b0bc749bcec9ee987
Ali <alibilge@yahoo.ca>
2020-03-30 13:08:25 -0600

    Challenge Assignments

---
+++ js/removeLeadingTrailing.js
@@ -0,0 +1,5 @@
+// This is a removing 0's from leading and tailings.
+
+function removeLeadingTrailing(n) {
+    return parseFloat(n).toString(); // Returns a floating point number and coverts number to string.
+}
